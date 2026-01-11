dbe4edf37f14faed7fa11c352b262366da4bbfbf
Oussama Amkhou <163328646+oussama-2000@users.noreply.github.com>
2026-01-11 15:21:46 +0100

    Refactor last_word.c for improved readability

--- last_word.c
+++ last_word.c
@@ -1,51 +1,37 @@
-
 #include <unistd.h>
 
-int ft_isspace(char c)
-{
-    return (c == 32 || (c >= 9 && c <= 13));
-}
-void ft_substr(char *str,int start,int end)
-{
-    int i = 0;
-    while(i < start)
-        i++;
-    while(i < end)
-    {
-        write(1,&str[i],1);
-        i++;
-    }
 
+int is_space(char c)
+{
+    return ((c == 32) || (c >= 9 && c <= 13));
 }
 
-int main(int argc, char **argv)
+
+int main(int ac, char **av)
 {
-    if(argc == 2)
+    if(ac == 2)
     {
+    
         int i = 0;
-        int end = 0;
-        int start = 0;
-        
-        while(ft_isspace(argv[1][i]))
+        while (av[1][i])
             i++;
-        if(argv[1][i] == '\0')
+        
+        i--;
+        while(is_space(av[1][i]))
+            i--;
+        if (!av[1][i])
         {
-            write(1,"\n",1);
+            write(1, "\n", 1);
             return 0;
         }
-        while(argv[1][i] != '\0')
+        while(!is_space(av[1][i]))
+            i--;
+        i++;
+        while(av[1][i] && !is_space(av[1][i]))
+        {
+            write(1,&av[1][i],1);
             i++;
-        i--;
-        while (i >= 0 && ft_isspace(argv[1][i]))
-            i--;
-        end = i+1;
-        while(i >= 0 && !ft_isspace(argv[1][i]))
-            i--;
-        start= i+1;
-        ft_substr(argv[1],start,end);
-        write(1,"\n",1);
+        }
     }
-    else
-        write(1,"\n",1);
-
+    write(1,"\n",1);
 }
