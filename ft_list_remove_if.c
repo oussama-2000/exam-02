fbe81d8b3b3ef6f87ef4744b9cb7c6446d3fc8b6
Oussama Amkhou <163328646+oussama-2000@users.noreply.github.com>
2026-01-15 01:16:50 +0100

    Refactor ft_list_remove_if function logic

--- ft_list_remove_if.c
+++ ft_list_remove_if.c
@@ -1,59 +1,27 @@
 
 #include "ft_list.h"
 
-t_list *lst_new(int data)
-{
-    t_list *node;
-    node = malloc(sizeof(t_list));
-    node->data = data;
-    node->next =  NULL;
-    return node;
-}
-int compare(int n1, int n2)
-{
-    if(n1 == n2)
-        return 0;
-    return 1;
-}
-void ft_list_remove_if(t_list **begin_list, int data_ref, int (*cmp)())
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
     t_list *prev = *begin_list;
-    if(cmp(prev->data,data_ref) == 0)
-    {
-        *begin_list = prev->next;
-        free(prev);
-    }
     t_list *cur = prev->next;
 
-    while(cur)
+    if (cmp(prev->data, data_ref) == 0)
     {
-        if (cmp(cur->data, data_ref) == 0)
+        *begin_list = cur;
+        free(prev);
+    }
+    else
+    {
+        while (prev->next)
         {
-            prev->next = cur->next;
-            free(cur);
-            cur = prev->next;
-        }
-        else
-        {
-            prev = cur;
-            cur = cur->next;
+            if (cmp(prev->data, data_ref) == 0)
+            {
+                cur = prev->next;
+                free(prev);
+                prev = cur;
+            }
+            prev = prev->next;
         }
     }
-    
 }
-
-// int main()
-// {
-//     t_list *node1 = lst_new(1);
-//     t_list *node2 = lst_new(2);
-//     t_list *node3 = lst_new(3);
-
-//     node1->next = node2;
-//     node2->next = node3;
-//     ft_list_remove_if(&node1,3,compare);
-//     while(node1)
-//     {
-//         printf("%d\n",node1->data);
-//         node1 = node1->next;
-//     }
-// }
